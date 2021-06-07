9cabf18e13d17a8e343aa0eb7aec7da53363dd3a
Luca <rossiluca711@gmail.com>
2021-06-07 17:27:45 +0200

    first full code commit

---
+++ classifier_emb.py
@@ -0,0 +1,67 @@
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+import numpy as np
+from sklearn.neighbors import KDTree
+from config import *
+
+
+CLASS_VECTORS = list()
+
+
+def custom_kernel_init(shape, dtype=None):
+    return CLASS_VECTORS.T
+
+
+class EmbeddedClassifier:
+    def __init__(self, signatures, input_shape=INPUT_SHAPE, learning_rate=LEARNING_RATE_CLS, beta=BETA):
+        self.model = self.__build_model(signatures, input_shape, learning_rate, beta)
+
+    def __build_model(self, signatures, input_shape, learning_rate, beta):
+        global CLASS_VECTORS
+        CLASS_VECTORS = signatures
+        model = Sequential()
+        model.add(Dense(signatures.shape[1], input_shape=(input_shape,), activation='relu', kernel_initializer=RandomNormal(stddev=0.02)))
+        model.add(Dense(signatures.shape[0], activation='softmax', trainable=False, kernel_initializer=custom_kernel_init))
+        print('-----------------------')
+        print('Classifier')
+        model.summary()
+        adam = Adam(lr=learning_rate, beta_1=beta)
+        model.compile(loss=SparseCategoricalCrossentropy(from_logits=True), optimizer=adam, metrics=['accuracy'])
+        return model
+
+    def get_model(self):
+        return self.model
+
+    def train(self, trainX, trainY, n_batch=N_BATCH_CLS, n_epochs=N_EPOCHS_CLS):
+        self.model.fit(trainX, trainY, verbose=2, epochs=n_epochs, batch_size=n_batch, shuffle=True)
+
+    def eval(self, x, y, classnames, signatures):
+        print()
+        #score = self.model.evaluate(x, y, verbose=0)
+        #print('Test loss:', score[0])
+        #print('Test accuracy:', score[1])
+        inp = self.model.input
+        out = self.model.layers[-2].output
+        model = Model(inp, out)
+        predY = model.predict(x)
+        tree = KDTree(signatures)
+        top5, top3, top1 = 0, 0, 0
+        for i, pred in enumerate(predY):
+            pred = np.expand_dims(pred, axis=0)
+            dist_5, index_5 = tree.query(pred, k=5)
+            # TODO fix index_5 !!!
+            pred_labels = [classnames[index] for index in index_5[0]]
+            true_label = y[i]
+            if true_label in pred_labels:
+                top5 += 1
+            if true_label in pred_labels[:3]:
+                top3 += 1
+            if true_label == pred_labels[0]:
+                top1 += 1
+        print("ZERO SHOT LEARNING SCORE")
+        print("-> Top-5 Accuracy: %.2f" % (top5 / float(len(x))))
+        print("-> Top-3 Accuracy: %.2f" % (top3 / float(len(x))))
+        print("-> Top-1 Accuracy: %.2f" % (top1 / float(len(x))))
---
+++ config.py
@@ -0,0 +1,97 @@
+COLAB = True
+GAN = True
+LOAD = True
+GZSL = True
+
+
+DATASET_CUB = 'cub'
+DATASET_AWA = 'awa'
+DATASET_SUN = 'sun'
+DATASET = DATASET_AWA
+
+
+PARAMS = {
+    DATASET_CUB: {
+        'file_separator': ' ',
+        'input_shape': 2048,
+        'n_classes': 200,
+        'n_unseen_classes': 50,
+        'n_attributes': 312,
+        'n_epochs': 70,
+        'n_epochs_cls': 25,
+        'batch_size': 64,
+        'batch_size_cls': 64,
+        'learning_rate': 0.0001,
+        'learning_rate_cls': 0.001,
+        'beta': 0.5,
+        'syn_num': 300,
+        'gradient_penalty_weight': 10,
+        'cls_loss_weight': 0.01,
+        'n_critic': 5,
+        'latent_dim': 312
+    },
+    DATASET_AWA: {
+        'file_separator': '\t',
+        'input_shape': 2048,
+        'n_classes': 50,
+        'n_unseen_classes': 10,
+        'n_attributes': 85,
+        'n_epochs': 30,
+        'n_epochs_cls': 25,
+        'batch_size': 64,
+        'batch_size_cls': 64,
+        'learning_rate': 0.00001,
+        'learning_rate_cls': 0.001,
+        'beta': 0.5,
+        'syn_num': 1800,
+        'gradient_penalty_weight': 10,
+        'cls_loss_weight': 0.01,
+        'n_critic': 5,
+        'latent_dim': 85
+    },
+    DATASET_SUN: {
+        'file_separator': '\t',
+        'input_shape': 2048,
+        'n_classes': 717,
+        'n_unseen_classes': 72,
+        'n_attributes': 102,
+        'n_epochs': 40,
+        'n_epochs_cls': 25,
+        'batch_size': 64,
+        'batch_size_cls': 64,
+        'learning_rate': 0.0002,
+        'learning_rate_cls': 0.001,
+        'beta': 0.5,
+        'syn_num': 400,
+        'gradient_penalty_weight': 10,
+        'cls_loss_weight': 0.01,
+        'n_critic': 5,
+        'latent_dim': 102
+    },
+}
+
+INPUT_SHAPE = PARAMS[DATASET]['input_shape']
+N_CLASSES = PARAMS[DATASET]['n_classes']
+N_UNSEEN_CLASSES = PARAMS[DATASET]['n_unseen_classes']
+N_ATTRIBUTES = PARAMS[DATASET]['n_attributes']
+
+LATENT_DIM = PARAMS[DATASET]['latent_dim']
+N_CRITIC = PARAMS[DATASET]['n_critic']
+N_EPOCHS_GAN = PARAMS[DATASET]['n_epochs']
+N_EPOCHS_CLS = PARAMS[DATASET]['n_epochs_cls']
+N_BATCH_GAN = PARAMS[DATASET]['batch_size']
+N_BATCH_CLS = PARAMS[DATASET]['batch_size_cls']
+LEARNING_RATE = PARAMS[DATASET]['learning_rate']
+LEARNING_RATE_CLS = PARAMS[DATASET]['learning_rate_cls']
+BETA = PARAMS[DATASET]['beta']
+
+GRADIENT_PENALTY_WEIGHT = PARAMS[DATASET]['gradient_penalty_weight']
+CLS_LOSS_WEIGHT = PARAMS[DATASET]['cls_loss_weight']
+N_SAMPLES_GAN = N_UNSEEN_CLASSES * PARAMS[DATASET]['syn_num']
+
+PATH = '/content/data' if COLAB else 'C:/Users/lucar/Desktop/data'
+FEATURES_PATH = PATH + '/' + DATASET + '/res101.mat'
+ATTRIBUTES_PATH = PATH + '/' + DATASET + '/att_splits.mat'
+FILE_SEPARATOR = PARAMS[DATASET]['file_separator']
+MODEL_SAVE_PATH = 'cgan_generator.h5'
+SAVE_MODEL_EVERY = 10
